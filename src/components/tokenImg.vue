52c35a5a3199ce5361e4afeb6b96293c5be3b3ff
missop <html.css.tscript@gmail.com>
2025-05-13 15:50:20 +0800

    feat: remove yarn.lock

---
+++ src/components/tokenImg.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+defineProps({
+  row: {
+    type: Object,
+    default: () => ({})
+  },
+  tokenClass: {
+    type: String,
+    default: 'w-24px h-24px'
+  },
+  chainClass: {
+    type: String,
+    default: 'w-12px h-12px'
+  }
+})
+const {token_logo_url} = useConfigStore()
+</script>
+
+<template>
+  <div class="relative">
+    <el-image
+      :class="`mr-3px rounded-full  block ${tokenClass}`"
+      :style="{
+                  display:'block'
+       }"
+      :src="`${token_logo_url}${row.logo_url}`"
+    >
+      <template #error>
+        <img class="w-full block" src="@/assets/images/icon-default.png" alt="">
+      </template>
+    </el-image>
+    <img
+      v-if="row.chain"
+      :class="`rounded-full absolute right-0 bottom-0 block ${chainClass}`"
+      :src="`${token_logo_url}chain/${row.chain}.png`"
+      alt=""
+    >
+  </div>
+</template>
+
+<style scoped>
+
+</style>
