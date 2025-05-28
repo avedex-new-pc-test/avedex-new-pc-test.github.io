6321e0970950c8f4265174c9470c0a6c09e54229
Lakes <sakirajessica01@gmail.com>
2025-05-28 09:53:41 +0800

    feat: update search icon

--- src/pages/token/components/belowChartTable/transactions/transactions.vue
+++ src/pages/token/components/belowChartTable/transactions/transactions.vue
@@ -533,7 +533,7 @@ function onRowClick({rowData}: RowEventHandlerParams) {
   tokenDetailSStore.$patch({
     drawerVisible: true,
     tokenInfo: {
-      id: route.params.id!,
+      id: route.params.id! as string,
       symbol,
       logo_url,
       chain,
