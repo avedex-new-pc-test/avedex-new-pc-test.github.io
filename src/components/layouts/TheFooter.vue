7cf63829a4cda59648c1b1804abf64a1136dfc4d
lewis <691068928@qq.com>
2025-05-29 02:30:24 +0800

    feat: ui

--- src/components/layouts/TheFooter.vue
+++ src/components/layouts/TheFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="h-50px bg-[--d-222-l-F2F2F2] absolute bottom-0 left-0 w-full">footer</footer>
+  <footer class="h-40px bg-[--d-222-l-F2F2F2]  w-full">footer</footer>
 </template>
 
 <script setup lang='ts'>
