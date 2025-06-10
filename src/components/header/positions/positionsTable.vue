596c9fc4a282a7eed9f481e3f00fadf3879c3b9c
lewis <691068928@qq.com>
2025-06-10 17:43:22 +0800

    fix:

--- src/components/header/positions/positionsTable.vue
+++ src/components/header/positions/positionsTable.vue
@@ -44,7 +44,7 @@
           v-infinite-scroll="fetchTable"
           class="text-0 lh-0 h-0"
           :infinite-scroll-disabled="paginationParams.loaded || paginationParams.finished"
-          :infinite-scroll-distance="0"
+          :infinite-scroll-distance="20"
           :infinite-scroll-delay="200"
           :infinite-scroll-immediate="true"
         />
@@ -213,6 +213,7 @@ onMounted(() => {
   --el-table-header-bg-color:transparent;
   --el-table-header-text-color:var(--d-999-l-666);
   --el-table-text-color:var(--d-F5F5F5-l-222);
+  --el-table-row-hover-bg-color:var(--d-33353D-l-F5F5F5);
   --el-table-border: 0.5px solid var(--d-33353D-l-F5F5F5);
   :deep() thead{
     font-size: 10px;
