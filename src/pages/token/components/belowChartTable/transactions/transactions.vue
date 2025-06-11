91e009d15f3d0b619a743e3461a07feeecb18a7e
missop <html.css.tscript@gmail.com>
2025-06-11 17:34:16 +0800

    fix: update position profit_ratio

--- src/pages/token/components/belowChartTable/transactions/transactions.vue
+++ src/pages/token/components/belowChartTable/transactions/transactions.vue
@@ -867,8 +867,8 @@ function resetMakerAddress() {
               </div>
             </UserRemark>
             <Icon
-              :name="tableFilter.markerAddress?'codicon:filter-filled':'codicon:filter'"
-              :class="`${tableFilter.markerAddress ? 'color-#3F80F7' : 'color-[--d-666-l-999]'} cursor-pointer text-12px shrink-0`"
+              name="custom:filter"
+              :class="`${tableFilter.markerAddress ? 'color-#3F80F7' : 'color-[--d-666-l-999]'} cursor-pointer text-10px shrink-0`"
               @click.self.stop="setMakerAddress(row.wallet_address)" />
           </div>
         </template>
