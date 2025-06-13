a5022f3ef82d508e0dde7558157a12cb23d8f9d4
Lakes <sakirajessica01@gmail.com>
2025-06-13 17:48:38 +0800

    feat: sort overview

--- src/pages/token/components/right/overview.vue
+++ src/pages/token/components/right/overview.vue
@@ -2,6 +2,10 @@
   <div>
     <div class="text-16px py-12px color-#999">{{ $t('tokenInfo') }}</div>
     <ul class="text-12px mt-10px">
+      <li class="flex justify-between mb-12px">
+        <span class="color-[--d-666-l-999]">{{ $t('name') }}</span>
+        <span class="color-[--d-999-l-666]">{{ token?.name || '-' }}</span>
+      </li>
       <li v-if="token?.token" class="flex justify-between mb-12px">
         <span class="color-[--d-666-l-999]">{{ $t('token') }}</span>
         <div class="flex items-center justify-end color-[--d-999-l-666]">
@@ -10,9 +14,36 @@
           <Icon v-copy="token?.token" name="bxs:copy" class="ml-5px clickable" />
         </div>
       </li>
+      <li v-if="pair" class="flex justify-between mb-12px">
+        <span class="color-[--d-666-l-999]">{{ $t('pair') }}</span>
+        <div class="flex items-center justify-end color-[--d-999-l-666]">
+          <a class="clickable color-[--d-999-l-666] hover:color-[--d-F5F5F5-l-333] text-decoration-none"  :href="formatExplorerUrl(token?.chain as string, tokenStore?.pairAddress, 'address')" target="_blank"> {{ formatAddress(tokenStore?.pairAddress) }}</a>
+          <Icon v-copy="tokenStore?.pairAddress" name="bxs:copy" class="ml-5px clickable" />
+        </div>
+      </li>
+      <li v-if="checkStore.checkResult?.creator_address" class="flex justify-between mb-12px">
+        <span class="color-[--d-666-l-999]">{{ $t('contractCreator') }}</span>
+        <div class="flex items-center justify-end color-[--d-999-l-666]">
+          <a class="clickable color-[--d-999-l-666] hover:color-[--d-F5F5F5-l-333] text-decoration-none"  :href="formatExplorerUrl(token?.chain as string, checkStore.checkResult.creator_address, 'address')" target="_blank"> {{ formatAddress(checkStore.checkResult?.creator_address || '') }}</a>
+          <Icon v-copy="checkStore.checkResult.creator_address || ''" name="bxs:copy" class="ml-5px clickable" />
+        </div>
+      </li>
+      <li v-if="owner" class="flex justify-between mb-12px">
+        <span class="color-[--d-666-l-999]">{{ $t('contractOwner') }}</span>
+        <div class="flex items-center justify-end color-[--d-999-l-666]">
+          <a class="clickable color-[--d-999-l-666] hover:color-[--d-F5F5F5-l-333] text-decoration-none"  :href="formatExplorerUrl(token?.chain as string, owner, 'address')" target="_blank"> {{ formatAddress(owner) }}</a>
+          <Icon v-copy="checkStore?.checkResult?.owner || token?.owner || ''" name="bxs:copy" class="ml-5px clickable" />
+          <Icon style="display: none;" name="custom:checked" />
+        </div>
+      </li>
+      <li v-if="token?.total" class="flex justify-between mb-12px">
+        <span class="color-[--d-666-l-999]">{{ $t('mcap') }}</span>
+        <span class="color-[--d-999-l-666]">{{ formatNumber(tokenStore?.marketCap || 0) }}</span>
+      </li>
       <li class="flex justify-between mb-12px">
-        <span class="color-[--d-666-l-999]">{{ $t('name') }}</span>
-        <span class="color-[--d-999-l-666]">{{ token?.name || '-' }}</span>
+        <span class="color-[--d-666-l-999]">{{ $t('FDV') }}</span>
+        <span class="color-[--d-999-l-666]">{{ formatNumber(Number(token?.total || 0) * (tokenStore?.price || 0))
+          }}</span>
       </li>
       <!-- <li class="flex justify-between mb-10px">
           <span class="color-[--d-666-l-999]">{{ $t('createdTime') }}</span>
@@ -26,29 +57,9 @@
         <span class="color-[--d-666-l-999]">{{ $t('circulation') }}</span>
         <span class="color-[--d-999-l-666]">{{ formatNumber(tokenStore?.circulation.toFixed() || 0) }}</span>
       </li>
-      <li v-if="token?.total" class="flex justify-between mb-12px">
-        <span class="color-[--d-666-l-999]">{{ $t('mcap') }}</span>
-        <span class="color-[--d-999-l-666]">{{ formatNumber(tokenStore?.marketCap || 0) }}</span>
-      </li>
-      <li class="flex justify-between mb-12px">
-        <span class="color-[--d-666-l-999]">{{ $t('FDV') }}</span>
-        <span class="color-[--d-999-l-666]">{{ formatNumber(Number(token?.total || 0) * (tokenStore?.price || 0))
-          }}</span>
-      </li>
-      <li v-if="checkStore.checkResult?.creator_address" class="flex justify-between mb-10px">
-        <span class="color-[--d-666-l-999]">{{ $t('contractCreator') }}</span>
-        <div class="flex items-center justify-end color-[--d-999-l-666]">
-          <a class="clickable color-[--d-999-l-666] hover:color-[--d-F5F5F5-l-333] text-decoration-none"  :href="formatExplorerUrl(token?.chain as string, checkStore.checkResult.creator_address, 'address')" target="_blank"> {{ formatAddress(checkStore.checkResult?.creator_address || '') }}</a>
-          <Icon v-copy="checkStore.checkResult.creator_address || ''" name="bxs:copy" class="ml-5px clickable" />
-        </div>
-      </li>
-      <li v-if="owner" class="flex justify-between mb-10px">
-        <span class="color-[--d-666-l-999]">{{ $t('contractOwner') }}</span>
-        <div class="flex items-center justify-end color-[--d-999-l-666]">
-          <a class="clickable color-[--d-999-l-666] hover:color-[--d-F5F5F5-l-333] text-decoration-none"  :href="formatExplorerUrl(token?.chain as string, owner, 'address')" target="_blank"> {{ formatAddress(owner) }}</a>
-          <Icon v-copy="checkStore?.checkResult?.owner || token?.owner || ''" name="bxs:copy" class="ml-5px clickable" />
-          <Icon style="display: none;" name="custom:checked" />
-        </div>
+      <li v-if="pair" class="flex justify-between mb-12px">
+        <span class="color-[--d-666-l-999]">{{ $t('createdTime') }}</span>
+        <span class="color-[--d-999-l-666]">{{ pair?.created_at ? formatDate(pair?.created_at) : '-' }}</span>
       </li>
       <!-- <template v-for="(item, index) in medias?.slice()" :key="index">
         <template v-if="item?.url">
@@ -67,7 +78,7 @@
         </template>
       </template> -->
     </ul>
-    <ul v-if="pair" class="text-12px mt-10px">
+    <!-- <ul v-if="pair" class="text-12px mt-10px">
       <li class="flex justify-between mb-12px">
         <span class="color-[--d-666-l-999]">{{ $t('pair') }}</span>
         <div class="flex items-center justify-end color-[--d-999-l-666]">
@@ -79,7 +90,7 @@
         <span class="color-[--d-666-l-999]">{{ $t('createdTime') }}</span>
         <span class="color-[--d-999-l-666]">{{ pair?.created_at ? formatDate(pair?.created_at) : '-' }}</span>
       </li>
-    </ul>
+    </ul> -->
     <div>
       <div  v-if="intro" class="text-14px mb-12px color-[--d-666-l-999]">{{ $t('currencyOverview') }}</div>
       <div class="text-12px color-[--d-999-l-666] token-description">
