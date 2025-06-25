6bfe2cbbc762b1e28e3b397c3cc616ea7d80a536
lewis <691068928@qq.com>
2025-06-25 15:15:45 +0800

    feat

---
+++ src/components/monitor/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="">
+
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const { t } = useI18n()
+
+
+onMounted(async () => {
+})
+
+</script>
+
+<style scoped>
+
+</style>
