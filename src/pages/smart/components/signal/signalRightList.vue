e96d82719770848aa60d47bd42b750223c2257ee
missop <html.css.tscript@gmail.com>
2025-07-04 16:13:41 +0800

    fix: signal page

--- src/pages/smart/components/signal/signalRightList.vue
+++ src/pages/smart/components/signal/signalRightList.vue
@@ -138,9 +138,7 @@ function openDrawer(item: GetSignalV2ListResponse<IActionItem | IActionV3Item>) 
     :height="height-286"
     @scroll="onScroll"
   >
-    <div class="grid gap-4px"
-         :class="listData.length>1 ? 'grid-cols-[repeat(auto-fit,minmax(454px,1fr))]':'grid-cols-[454px]'"
-    >
+    <div class="flex flex-wrap gap-4px">
       <SignalRightItem
         v-for="(item,index) in listData"
         :class="item.actions.length > 3 ? 'border-#3F80F7':'hover:border-#3F80F7'"
