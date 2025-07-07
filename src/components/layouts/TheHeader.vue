d3fed93a43309c325d154b0857cdcd366afbc528
luweijun <luweij@gmail.com>
2025-07-07 10:41:16 +0800

    fix:change top menu

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -123,19 +123,8 @@ const list = computed(() => {
     {id: 'index', name: t('markets'), src: 'https://ave.ai/' + query, target: '_blank'},
     {id: 'pump', name: t('pump1'), src: 'https://ave.ai/pump' + query, target: '_blank'},
     {id: 'smart', name: t('smarter2'), src: '/smart', target: '_self'},
+    {id: 'balances', name: t('balances'), src: '/address', target: '_self'},
   ]
-  // 已经链接钱包且链则跳转到新版，否则跳到旧版，包括空白页面
-  if(botStore.accessToken){
-    const token = botStore.getWalletAddress('solana')
-    menues.push({
-      id: 'assets',
-      name: t('balances'),
-      src: `/address/${token}/solana`,
-      target: '_self'
-    })
-  } else {
-    menues.push({id: 'assets', name: t('balances'), src: 'https://ave.ai/address' + query, target: '_blank'})
-  }
   return menues
 })
 
